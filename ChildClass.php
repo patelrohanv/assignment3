9b6cc00a4f6121e35d08b00f8331524449ffd6db
patelrohanv <rohanpatel@Rohans-MacBook-Pro.local>
2017-03-25 23:49:31 -0400

    Stuff

--- ChildClass.php
+++ ChildClass.php
@@ -1,6 +1,29 @@
 <?php 
 	// this file will extend PArentClass.php
 
+	/*
+	You can have any number of methods that 
+	will in some way use the parent class 
+	but also implemennt it's own separate functionality
+
+	- at least 2 php magic methods
+	- clone the original object for later use
+	*/
 	class ChildClass extends ParentClass {
-		
+
+		static $instance;
+
+		function __construct(){
+			parent::__construct();
+			parent->numChildren = parent->numChildren + 1;
+		}
+
+		function __clone(){
+			this->instance = clone this->instance;
+		}
+
+		function countdown($num){
+			echo parent::countdownHeading($num);
+		}
 	}
+?>
--- ParentClass.php
+++ ParentClass.php
@@ -1,6 +1,24 @@
 <?php
 	// This is the file for the parent class
-
+	/*
+	-function(s) that will take any type of paramters you want 
+		work with those parameters and deliver some sort of output.
+	-make sure to use at least 2 php magic methods 
+	*/
 	class ParentClass {
-		
+		public $numChildren = 0;
+		public $thisCount;
+
+		public function countdownHeading($num){
+			echo "<h5> The Countdown from $num </h5><br>";
+		}
+
+		function __construct($count){
+			$thisCound = $count;
+		}
+
+		function __toString(){
+			echo "<p> Parent Class with $numChildren Children</p><br>";
+		}
 	}
+?>
